10b1996dd37b834839144c266e74901d5f0efb70
emyede <96616310+yanz17@users.noreply.github.com>
2025-05-31 14:44:46 +0700

    update some configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.vue',
+    './storage/framework/views/*.php', // tambahkan ini
+    './app/View/Components/**/*.php', 
   ],
   theme: {
     extend: {},
@@ -11,5 +13,8 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     themes: ['light'], // ubah ke 'light' agar teks tidak putih, atau bisa pakai 'corporate', 'lofi', dll
+    base: true,     // tambahkan ini
+    styled: true,   // tambahkan ini
+    utils: true,    // tambahkan ini
   },
 };
